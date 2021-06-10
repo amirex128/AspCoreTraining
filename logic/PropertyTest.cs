7fd5bb8b74cbadb0f60b585254b679d4246370ec
_Amirex_ <amirex128@gmail.com>
2021-06-10 23:32:57 +0430

    Add project files.

---
+++ logic/PropertyTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web1.logic
+{
+    public class PropertyTest
+    {
+        private string _name;
+        public string name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        
+        public string values { get; set; }
+    }
+}
